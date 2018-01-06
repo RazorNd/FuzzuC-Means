082f1d8947a205c382975c7935bdd8ae47bc6ef1
RazorNd <daniltmb@mail.ru>
2018-01-06 18:34:18 +0300

    Change Decision value calculation type

--- src/FuzzuC-Means/FuzzuCMeans.h
+++ src/FuzzuC-Means/FuzzuCMeans.h
@@ -13,6 +13,8 @@ class FuzzuCMeans
     double _m;
     double _epsilon;
     const Norma &_norma;
+
+    typedef std::vector<DistanceCalculator::DistanceVector> V_t;
 public:
     FuzzuCMeans(unsigned int clustersCounts, double m, double epsilon, const Norma &norma = EvklidDistance()) :
         _clustersCounts(clustersCounts),
@@ -28,7 +30,7 @@ public:
         ClustersStorage clusters(_clustersCounts, getSize(X));
         double previousDecisionValue = 0.0, currentDesisionValue = 1.0;
         DistanceCalculator distanceCalculator(_norma);
-        std::vector<DistanceCalculator::DistanceVector> distancesToCenters(_clustersCounts);
+        V_t distancesToCenters(_clustersCounts);
 
         while (std::abs(currentDesisionValue - previousDecisionValue) > _epsilon) {
             std::swap(currentDesisionValue, previousDecisionValue);
@@ -42,10 +44,32 @@ public:
                         std::bind(distanceCalculator, std::placeholders::_1, X)
                         );
 
-            currentDesisionValue = clusters.updatedClusterForDistances(distancesToCenters, _m);
+            clusters.updatedClusterForDistances(distancesToCenters, _m);
+
+
+            currentDesisionValue = calculateDecisionValue(clusters.getClusters(), centers, X);
         }
 
         return clusters.getClusters();
+    }
+
+private:
+    template<class T>
+    double calculateDecisionValue(const ClustersStorage::ClusterVecotor &clusters, const VectorX &V, const T& X) const
+    {
+        auto c = V.size();
+        auto l = clusters.size();
+
+        double result = 0.0;
+
+        for (size_t i = 0; i < c; i++) {
+            for (size_t j = 0; j < l; j++) {
+                auto distance = _norma(V[i], getValue(X, j));
+                result += std::pow(clusters[i][j], _m) * (distance * distance);
+            }
+        }
+
+        return result;
     }
 };
 
